37e0d7e759cfd026e685d92975fdf94bf094fa5f
”Your <3514214835@qq.com>
2023-04-06 22:55:39 +0800

    Changes

--- src/hanshujieguo.java
+++ src/hanshujieguo.java
@@ -2,26 +2,30 @@ import java.util.Scanner;
 
 public class hanshujieguo {
     public static void main(String[] args) {
-        final double eps =10e-7;
-        Scanner wgl= new Scanner(System.in);
+        final double eps = 10e-7;
+        Scanner wgl = new Scanner(System.in);
         System.out.println("请输入方程的系数a、b、c:");
-        double a,b,c;
+        double a, b, c;
         a = wgl.nextInt();
         b = wgl.nextInt();
         c = wgl.nextInt();
         double d = b * b - 4 * a * c;
 
-        if (d > 0){
+        if (d > 0) {
             System.out.println("Two roots");
-            double x1 = (-b + Math.sqrt(d) /(2 * a));
+            double x1 = (-b + Math.sqrt(d) / (2 * a));//Math.sprt 平方跟
             System.out.println(x1);
-            double x2 = (-b - Math.sqrt(d) /(2 * a));
+            double x2 = (-b - Math.sqrt(d) / (2 * a));
             System.out.println(x2);
-        } else if (d == 0) {
+        }
+        else if (d == 0)
+        {
             System.out.println("One root");
             double x = -b / (2 * a);
             System.out.println(x);
-        }else {
+        }
+        else
+        {
             System.out.println("No roots");
         }
     }
--- src/zuoye2.java
+++ src/zuoye2.java
@@ -1,8 +1,8 @@
 public class zuoye2 {
     public static void main(String[] args) {
-        int count = 0,i;
+        int count = 0, i;
         boolean flag;
-        for(i = 101; i<201; i++) {
+        for(i = 101; i < 201; i++) {
             flag = true;
             for (int x = 2; x < i; x++) {
                 if (i % x == 0) {
