d258628d3843400ab1daf44925caa142c50b2be4
”Your <3514214835@qq.com>
2023-04-06 22:55:39 +0800

    Changes

--- src/lianxi1.java
+++ src/lianxi1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class lianxi1 {
     public static void main(String[] args) {
-
+        Scanner x = new Scanner(System.in);
+        System.out.println("请输入要判断数");
+        int i = x.nextInt();
+        int n = 0;
+        for (n = 2; n <= i - 1; n++){
+        if(i % n == 0){
+            System.out.println(i +"不是素数");
+            break;
+        }
+         }
+        if(n == i){
+            System.out.println(i +"是素数");
+        }
     }
+
 }
